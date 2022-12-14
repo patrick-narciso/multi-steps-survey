73258b1268be3e63945cb790e8eef67b98477a95
Patrick Narciso <patrick.ncsilva@gmail.com>
2022-12-14 20:42:53 -0300

    refactor: removes react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './components/pages/Home';
-import NotFound from './components/pages/NotFound';
-
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
+  return <h1>hello world</h1>;
 }
 
-function WrappedApp() {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-}
-
-export { WrappedApp, App };
+export default App;
