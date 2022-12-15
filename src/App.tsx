2be0f37f32bccb685f9f61683b3f299f12fff55c
Patrick Narciso <patrick.ncsilva@gmail.com>
2022-12-15 01:33:37 -0300

    refactor: changes atomic design pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-import { Checkbox, CheckboxGroup, Stack } from '@chakra-ui/react';
-
 function App() {
-  return (
-    <>
-      <Checkbox value="naruto">Naruto</Checkbox>
-      <Checkbox value="sasuke">Sasuke</Checkbox>
-      <Checkbox value="kakashi">Kakashi</Checkbox>
-    </>
-  );
+  return <h1>Hello world!</h1>;
 }
 
 export default App;
