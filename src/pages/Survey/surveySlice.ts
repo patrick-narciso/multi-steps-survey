727aba3ef90cd9ba2538dfbe840641e2e43dadbb
Patrick Narciso <patrick.ncsilva@gmail.com>
2022-12-15 03:20:11 -0300

    feat: setup redux toolkit and slices for survey

---
+++ src/pages/Survey/surveySlice.ts
@@ -0,0 +1,59 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable import/no-extraneous-dependencies */
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface SurveyState {
+  name?: string | null;
+  email?: string | null;
+  age: number;
+  gender: string;
+  favoriteBook: string;
+  favoriteColors: string[] | string;
+}
+
+export interface Identity {
+  name: string | null;
+  email: string | null;
+}
+
+export interface Details {
+  age: number;
+  gender: string;
+}
+
+export interface Favorites {
+  favoriteBook: string;
+  favoriteColors: string[] | string;
+}
+
+const initialState: SurveyState = {
+  name: '',
+  email: '',
+  age: 0,
+  gender: '',
+  favoriteBook: '',
+  favoriteColors: [],
+};
+
+export const surveySlice = createSlice({
+  name: 'survey',
+  initialState,
+  reducers: {
+    addIdentity: (state, action: PayloadAction<Identity>) => {
+      state.name = action.payload.name;
+      state.email = action.payload.email;
+    },
+    addDetails: (state, action: PayloadAction<Details>) => {
+      state.age = action.payload.age;
+      state.gender = action.payload.gender;
+    },
+    addFavorites: (state, action: PayloadAction<Favorites>) => {
+      state.favoriteBook = action.payload.favoriteBook;
+      state.favoriteColors = action.payload.favoriteColors;
+    },
+  },
+});
+
+export const { addIdentity, addDetails, addFavorites } = surveySlice.actions;
+
+export default surveySlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import surveyReducer from '../pages/Survey/surveySlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    survey: surveyReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
