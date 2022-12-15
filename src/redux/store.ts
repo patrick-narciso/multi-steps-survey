ddc360ca13a6b32e97ed1ae181b4b9ff8ffb4433
Patrick Narciso <patrick.ncsilva@gmail.com>
2022-12-15 02:43:52 -0300

    chore: setup redux toolkit

---
+++ src/redux/store.ts
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {},
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
