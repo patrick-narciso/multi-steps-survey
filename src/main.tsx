db70784767ac83892af1fe8765f01842b24b9105
Patrick Narciso <patrick.ncsilva@gmail.com>
2022-12-17 17:04:26 -0300

    refactor: removes root id element

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { store, persistor } from './redux/store';
 import App from './App';
 import theme from './theme';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.body as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
